ff16a078973928e7402e604b83c2a271b578ec62
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-07-18 17:41:02 +0400

    impl-ed task10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +25,70 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+var (
+	timeout time.Duration
+)
 
+func tcpConnection(host string, port string) error {
+	conn, err := net.DialTimeout("tcp", host+":"+port, timeout)
+
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	sigChan := make(chan os.Signal, 1)
+
+	signal.Notify(sigChan, os.Interrupt)
+	doneChan := make(chan struct{})
+
+
+	go func() {
+		_, err := io.Copy(conn, os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		}
+		conn.Close()
+		close(doneChan)
+	}()
+
+
+	go func() {
+		_, err := io.Copy(os.Stdout, conn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		}
+		close(doneChan)
+	}()
+
+	select {
+	case <-sigChan:
+		return fmt.Errorf("interrupted")
+	case <-doneChan:
+		fmt.Println("Connection closed, shutting down...")
+	}
+	return nil
+}
+
+func run() error {
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "timeout")
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 2 {
+		return fmt.Errorf("expected host and port, got %v", args)
+	}
+
+	host := args[0]
+	port := args[1]
+
+	return tcpConnection(host, port)
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
